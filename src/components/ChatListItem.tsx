5af17de10caab3b2902173e1fafcaccd930fb0f4
Livia Pessanha <liviapessanha99@gmail.com>
2024-05-17 12:02:32 -0300

    fixed problems with chatListItem layout

--- src/components/ChatListItem.tsx
+++ src/components/ChatListItem.tsx
@@ -6,18 +6,17 @@ type Props = {
 }
 export const ChatListItem = ({item, handleComponents}: Props) => {
   return (
-    <div className="bg-white flex cursor-pointer items-center ml-1">
-        <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="" className="w-10 h-10 rounded-full mr-2" />
-        <div className="w-full flex flex-col flex-1 justify-center border-b p-2  border-gray-200"> {/*chatListItem-lines */}
+    <div className="flex cursor-pointer items-center gap-2 w-full border-b p-2 border-gray-300 hover:bg-teal-100">
+        <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="" className="w-10 h-10 rounded-full" />
+        <div className="w-full flex overflow-hidden flex-col justify-center"> {/*chatListItem-lines */}
             <div onClick={handleComponents} 
-                className="h-10 flex justify-between items-center w-full md:pointer-events-none md:cursor-not-allowed"
-            
+                className="h-10 flex justify-between items-center w-full  md:pointer-events-none md:cursor-not-allowed"
             > {/*chatListItem-line */}
                 <div className="text-sm text-black">Bonieky Lacerda</div> {/*chatListItem-name */}
-                <div className="text-xs text-black/50">19:00</div> {/*chatListItem-date */}
+                <div className="text-xs text-black/50 ">19:00</div> {/*chatListItem-date */}
             </div>
-            <div> {/*chatListItem-line */}
-                <p className="text-sm text-black/40 flex justify-between items-center w-full">Opa, tudo bem?</p> {/*chatListItem-lastmsg */}
+            <div className="w-full"> {/*chatListItem-line */}
+                <p className="text-sm text-black/40 truncate w-full">Pariatur mollit in u Pariatur mollit in u Pariatur mollit in u Pariatur mollit in u</p> {/*chatListItem-lastmsg */}
             </div>
         </div>
     </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -17,14 +17,14 @@ export const SideBar = ({hiddenSideBar, handleComponents}: Props) => {
     ]);
 
   return (
-    <div className={`${!hiddenSideBar ? 'hidden' : 'block'}  md:w-1/3 w-full flex flex-col border-r border-gray-300`}>
-        <header className="h-14 flex justify-between items-center px-1 md:px-4">
+    <div className={`${!hiddenSideBar ? 'hidden' : 'block'}  md:w-1/3 w-full  flex flex-col md:border-r  border-gray-300`}>
+        <header className="h-14 w-full flex justify-between items-center  md:px-4 bg-teal-500 ">
             <img 
                 src="https://www.w3schools.com/howto/img_avatar2.png" 
                 alt="avatar" 
-                className="h-7 w-7 rounded-full cursor-pointer"
+                className="h-7 w-7 rounded-full cursor-pointer ml-1 md:ml-0"
             />
-            <div className="flex items-center">
+            <div className="flex  items-center">
                 <div className="w-7 h-7 cursor-pointer flex items-center">
                     <MdDonutLarge />
                 </div>
@@ -36,8 +36,8 @@ export const SideBar = ({hiddenSideBar, handleComponents}: Props) => {
                 </div>
             </div>
         </header>
-        <div className="bg-white/70 border-b border-slate-100 py-1 px-2">
-            <div className="bg-white rounded-md flex items-center p-1 gap-1">
+        <div className="bg-white/70 border-b border-slate-100 py-1 px-2 bg-teal-500">
+            <div className="bg-white rounded-md flex items-center gap-1">
                 <CiSearch/>
                 <input 
                     type="search"
@@ -45,7 +45,7 @@ export const SideBar = ({hiddenSideBar, handleComponents}: Props) => {
                 />
             </div>
         </div>
-        <div className="flex-1 overflow-auto scrollbar-thin scrollbar-thumb-transparent">
+        <div className="w-full scrollbar-thin scrollbar-thumb-transparent">
             {chatList.map((item, key) => (
                 <ChatListItem handleComponents={handleComponents} key={key} item={item} />
             ))}
